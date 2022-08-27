8e1993de3a617b2b95d37f8b5dc796a47de23057
Hayati TASTAN, PhD in GeoIT <haytastan@gmail.com>
2022-08-27 19:47:03 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Merhaba CodeSandbox</h1>
+      <h2>Dr. Hayati TAŞTAN</h2>
+      <h2>27 Ağustos 2022</h2>
+    </div>
+  );
+}
